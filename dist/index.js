e642d3a895621be2bd73b4b4542aa6cdd2117231
alsk1369854 <alsk1369854@gmail.com>
2022-12-28 19:19:03 +0800

    version 1.0.2

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,17 @@
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.set = exports.remove = exports.hasKey = exports.getSize = exports.getKeys = exports.getFunction = exports.getEntries = exports.default = exports.clear = exports.call = exports.asyncCall = void 0;
-var _FunctionCaller2 = _interopRequireDefault(require("./FunctionCaller"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-var _FunctionCaller = new _FunctionCaller2.default((typeof window === "undefined" ? "undefined" : _typeof(window)) === 'object' && window || void 0);
-var _default = _FunctionCaller;
-exports.default = _default;
-var set = _FunctionCaller.set;
-exports.set = set;
-var call = _FunctionCaller.call;
-exports.call = call;
-var asyncCall = _FunctionCaller.asyncCall;
-exports.asyncCall = asyncCall;
-var remove = _FunctionCaller.remove;
-exports.remove = remove;
-var clear = _FunctionCaller.clear;
-exports.clear = clear;
-var hasKey = _FunctionCaller.hasKey;
-exports.hasKey = hasKey;
-var getKeys = _FunctionCaller.getKeys;
-exports.getKeys = getKeys;
-var getEntries = _FunctionCaller.getEntries;
-exports.getEntries = getEntries;
-var getFunction = _FunctionCaller.getFunction;
-exports.getFunction = getFunction;
-var getSize = _FunctionCaller.getSize;
-exports.getSize = getSize;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSize = exports.getFunction = exports.getEntries = exports.getKeys = exports.hasKey = exports.clear = exports.remove = exports.asyncCall = exports.call = exports.set = void 0;
+const FunctionCaller_1 = require("./FunctionCaller");
+const _FunctionCaller = new FunctionCaller_1.default((typeof window === 'object' && window) || this);
+exports.default = _FunctionCaller;
+exports.set = _FunctionCaller.set;
+exports.call = _FunctionCaller.call;
+exports.asyncCall = _FunctionCaller.asyncCall;
+exports.remove = _FunctionCaller.remove;
+exports.clear = _FunctionCaller.clear;
+exports.hasKey = _FunctionCaller.hasKey;
+exports.getKeys = _FunctionCaller.getKeys;
+exports.getEntries = _FunctionCaller.getEntries;
+exports.getFunction = _FunctionCaller.getFunction;
+exports.getSize = _FunctionCaller.getSize;
 //# sourceMappingURL=index.js.map
